c55ea40882bf2667bf6dcfcf7402617b5f2f4881
Ethan <ethan.frey.2020@stuent.lhschools.org>
2019-09-30 09:46:03 -0400

    FIRST COMMIT

---
+++ src/Magic8Ball.java
@@ -0,0 +1,15 @@
+public class Magic8Ball {
+    public static void main(String[]args){
+        //TODO DECLARE VARIABLES
+
+        //TODO INITIALIZE VARIABLES MANUALLY
+
+        //TODO ASK QUESTIONS
+
+        //TODO
+
+        //TODO PRINT RESULTS
+
+
+    }
+}
