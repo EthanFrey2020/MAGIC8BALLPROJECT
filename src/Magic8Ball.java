57c79ace957f22ebfeb017eecfc076d70e48df97
Ethan <ethan.frey.2020@stuent.lhschools.org>
2019-09-30 10:03:14 -0400

    ASKED QUESTIONS

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Magic8Ball {
+
+
     public static void main(String[]args){
         //TODO DECLARE VARIABLES
             Scanner keyboard;
@@ -10,10 +12,24 @@ public class Magic8Ball {
 
 
         //TODO INITIALIZE VARIABLES MANUALLY
+            keyboard = new Scanner(System.in);
+
+
+
+        //TODO IF/THEN STATEMENT LADDERS
+
+
 
         //TODO ASK QUESTIONS
+            System.out.println("When is your birth month? (number wise) ex. September is 9");
+                birthmonth = keyboard.nextDouble();
+            System.out.println("When is your birth date?");
+
 
         //TODO PRINT RESULTS
+
+
+
 
 
     }
