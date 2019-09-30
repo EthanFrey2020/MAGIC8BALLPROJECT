47c83d96357a9e96e0d3b8eba5426b8c58545e2c
Ethan <ethan.frey.2020@stuent.lhschools.org>
2019-09-30 09:52:07 -0400

    DECLARED VARIABLES

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -1,12 +1,17 @@
+import java.util.Scanner;
+
 public class Magic8Ball {
     public static void main(String[]args){
         //TODO DECLARE VARIABLES
+            Scanner keyboard;
+            int testing;
+            String questions;
+            double birthdate,birthmonth,age,numbersOfPets;
+
 
         //TODO INITIALIZE VARIABLES MANUALLY
 
         //TODO ASK QUESTIONS
-
-        //TODO
 
         //TODO PRINT RESULTS
 
