417a5b405b5f4f8c4f4cf14c292cdf4a4c7ea718
Ethan <ethan.frey.2020@stuent.lhschools.org>
2019-10-10 09:48:14 -0400

    If/Then ladder

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -23,14 +23,46 @@ public class Magic8Ball {
             String responseNine = "You shouldn't do that";
             String responseTen = "Negative Go Staters Buckaroo";
 
-
-
+         //TODO ASK QUESTIONS
+                System.out.println("Pick a number 1-100");
+                        number = keyboard.nextInt();
 
         //TODO IF/THEN STATEMENT LADDERS
+                if (number > 1 && number <= 10) {
+                        System.out.println(responseEight);
+            }
+                if (number > 11 && number <= 20) {
+                        System.out.println(responseFive);
+            }
+                if (number > 21 && number <=30) {
+                        System.out.println(responseFour);
+                }
+                if (number > 31 && number <=40) {
+                    System.out.println(responseNine);
+            }
+                if (number > 41 && number <=50) {
+                    System.out.println(responseOne);
+            }
+                if (number > 51 && number <=60) {
+                    System.out.println(responseSeven);
+            }
+                if (number > 61 && number <=70) {
+                    System.out.println(responseSix);
+            }
+                if (number > 71 && number <=80) {
+                    System.out.println(responseTen);
+            }
+                if (number > 81 && number <=90) {
+                    System.out.println(responseThree);
+            }
+                if (number > 91 && number <=100) {
+                    System.out.println(responseTwo);
+            }
 
 
 
-        //TODO ASK QUESTIONS
+
+
 
 
         //TODO PRINT RESULTS
